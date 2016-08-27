cfd30aaaf42dda44770fdab0a212196018a63484
GiantMing <971334483@qq.com>
2016-08-27 20:39:59 +0800

    删错了,本来想删 cookieparse的,删到 bodyparse 去了

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const session = require('express-session');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 
 // routes
